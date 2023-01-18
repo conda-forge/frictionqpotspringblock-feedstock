49ff5dc5bfc3c03eef4d2d65bd4caa4122532ae9
Tom de Geus <tom@geus.me>
2023-01-18 10:28:11 +0100

    debugging

--- recipe/install_python.sh
+++ recipe/install_python.sh
@@ -1,4 +1,15 @@
+echo "======="
 echo "CMAKE_ARGS = ${CMAKE_ARGS}"
+echo "======="
+
+echo "======="
 echo "printenv"
 printenv
+echo "======="
+
+echo "======="
+echo "env"
+env
+echo "======="
+
 $PYTHON -m pip install . -v
