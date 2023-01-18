8b094ae759abdccd75ce954a7f090e3e93787755
Tom de Geus <tom@geus.me>
2023-01-18 21:49:16 +0100

    trying patch

--- recipe/install_python.sh
+++ recipe/install_python.sh
@@ -1,15 +1,1 @@
-echo "======="
-echo "CMAKE_ARGS = ${CMAKE_ARGS}"
-echo "======="
-
-echo "======="
-echo "printenv"
-printenv
-echo "======="
-
-echo "======="
-echo "env"
-env
-echo "======="
-
 $PYTHON -m pip install . -v
